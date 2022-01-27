a4a32d1e5c02c3fae1fc58b906a240c5aa053054
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-27 18:11:43 +0000

    increased coverage of golden master test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -10,13 +10,20 @@ describe('Gilded Rose', function () {
             new Item('Item2', 1, 10),
             new Item('Item3', 2, 10),
             new Item('Item4', 4, 20),
+            new Item('Item5', -1, 4),
+            new Item('Item6', -1, 10),
             new Item('Aged Brie', 1, 10),
             new Item('Aged Brie', 2, 20),
             new Item('Aged Brie', 0, 10),
             new Item('Aged Brie', 5, 30),
+            new Item('Aged Brie', -1, 49),
             new Item('Backstage passes to a TAFKAL80ETC concert', 5, 4),
             new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10),
             new Item('Backstage passes to a TAFKAL80ETC concert', 4, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 6, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 3, 46),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 30),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 50),
             new Item('Sulfuras, Hand of Ragnaros', 0, 80),
             new Item('Sulfuras, Hand of Ragnaros', 10, 80)
         ];
@@ -29,13 +36,20 @@ describe('Gilded Rose', function () {
             new Item('Item2', -2, 5),
             new Item('Item3', -1, 6),
             new Item('Item4', 1, 17),
+            new Item('Item5', -4, 0),
+            new Item('Item6', -4, 4),
             new Item('Aged Brie', -2, 15),
             new Item('Aged Brie', -1, 24),
             new Item('Aged Brie', -3, 16),
             new Item('Aged Brie', 2, 33),
+            new Item('Aged Brie', -4, 50),
             new Item('Backstage passes to a TAFKAL80ETC concert', 2, 13),
             new Item('Backstage passes to a TAFKAL80ETC concert', -3, 0),
             new Item('Backstage passes to a TAFKAL80ETC concert', 1, 29),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 3, 50),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 12, 33),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 12, 50),
             new Item('Sulfuras, Hand of Ragnaros', 0, 80),
             new Item('Sulfuras, Hand of Ragnaros', 10, 80)
           ]);
@@ -43,7 +57,7 @@ describe('Gilded Rose', function () {
         expect(gildedRose).to.deep.equal(gildedRoseOutput);
     });
 
-    it('should not change the values of Sulfuras', function() {
+    it('Sulfuras values should not change', function() {
         const items: Array<Item> = [
             new Item('Sulfuras, Hand of Ragnaros', 5, 80),
             new Item('Sulfuras, Hand of Ragnaros', 10, 50),
@@ -57,6 +71,36 @@ describe('Gilded Rose', function () {
             new Item('Sulfuras, Hand of Ragnaros', 5, 80),
             new Item('Sulfuras, Hand of Ragnaros', 10, 50),
             new Item('Sulfuras, Hand of Ragnaros', 0, 80)
+        ]);
+        expect(gildedRose).to.deep.equal(gildedRoseOutput);
+    });
+
+    it('brie should increase quality by 1 if sellIn > 0', function() {
+        const items: Array<Item> = [
+            new Item('Aged Brie', 5, 5),
+            new Item('Aged Brie', 1, 10),
+        ];
+        const gildedRose = new GildedRose(items)
+        gildedRose.updateQuality();
+        const gildedRoseOutput = new GildedRose([
+            new Item('Aged Brie', 4, 6),
+            new Item('Aged Brie', 0, 11),
+        ]);
+        expect(gildedRose).to.deep.equal(gildedRoseOutput);
+    });
+
+    it('brie should increase quality by 2 if sellIn <= 0', function() {
+        const items: Array<Item> = [
+            new Item('Aged Brie', 0, 5),
+            new Item('Aged Brie', 0, 10),
+            new Item('Aged Brie', -5, 40)
+        ];
+        const gildedRose = new GildedRose(items)
+        gildedRose.updateQuality();
+        const gildedRoseOutput = new GildedRose([
+            new Item('Aged Brie', -1, 7),
+            new Item('Aged Brie', -1, 12),
+            new Item('Aged Brie', -6, 42),
         ]);
         expect(gildedRose).to.deep.equal(gildedRoseOutput);
     });
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,24 +1,13 @@
 import { Item, GildedRose } from '../app/gilded-rose';
 
 const items: Array<Item> = [
-    new Item('Item1', 5, 2),
-    new Item('Item2', 1, 10),
-    new Item('Item3', 2, 10),
-    new Item('Item4', 4, 20),
-    new Item('Aged Brie', 1, 10),
-    new Item('Aged Brie', 2, 20),
-    new Item('Aged Brie', 0, 10),
-    new Item('Aged Brie', 5, 30),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 4),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 4, 20),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', 10, 80)
+    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 50),
 ];
 
 const gildedRose = new GildedRose(items)
 gildedRose.updateQuality();
 gildedRose.updateQuality();
 gildedRose.updateQuality();
+
 
 console.log(gildedRose.items);
