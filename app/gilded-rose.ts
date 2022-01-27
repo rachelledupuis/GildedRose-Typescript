209f63856a3e51c3bccdf329a698a9a3e7772a3f
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-27 19:05:31 +0000

    Refactored backstage passes

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -31,8 +31,23 @@ export class GildedRose {
                 }
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
+            //BACKSTAGE PASSES
+            if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.items[i].quality < 50 && this.items[i].sellIn >= 0) {
+                    this.items[i].quality = this.items[i].quality + 1;
+                    if (this.items[i].quality < 50 && this.items[i].sellIn < 11) {
+                        this.items[i].quality = this.items[i].quality + 1;
+                        if (this.items[i].quality < 50 && this.items[i].sellIn < 6) {
+                            this.items[i].quality = this.items[i].quality + 1;
+                        }
+                    }
+                } else if (this.items[i].sellIn < 0) {
+                    this.items[i].quality = 0;
+                }
+                this.items[i].sellIn = this.items[i].sellIn - 1;
+            }
 
-            if (this.items[i].name !== 'Aged Brie') {
+            if (this.items[i].name !== 'Aged Brie' && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     //decrease value for misc items that still have value
