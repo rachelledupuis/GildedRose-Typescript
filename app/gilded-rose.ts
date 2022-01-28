b0aa2bf8a965818c04d6a0f95e2875e45d546cd0
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-28 10:00:31 +0000

    Changed from for loop to forEach - only working for misc. items

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,57 +17,57 @@ export class GildedRose {
         this.items = items;
     }
 
-    increaseQualityByOne(n){
+   /*  increaseQualityByOne(n){
         this.items[n].quality = this.items[n].quality + 1;
     }
 
     decreaseSellInByOne(n){
         this.items[n].sellIn = this.items[n].sellIn - 1;
-    }
+    } */
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            const itemType = this.items[i].name;
+        this.items.forEach(function(item) {
+            const itemType = item.name;
             const sulfuras = 'Sulfuras, Hand of Ragnaros';
             const agedBrie = 'Aged Brie';
             const backstagePasses = 'Backstage passes to a TAFKAL80ETC concert';
-            const quality = this.items[i].quality
+            
             //SULFURAS
-            if (itemType === sulfuras) { continue; }
+            if (itemType == sulfuras) { }
             //AGED BRIE
-            if (itemType === agedBrie) {
-                if (this.items[i].quality < 50) {
-                    this.increaseQualityByOne(i);
-                    if (this.items[i].sellIn <= 0 && this.items[i].quality < 50) {
-                        this.increaseQualityByOne(i);
+            if (itemType == agedBrie) {
+                if (item.quality < 50) {
+                    item.sellIn = item.sellIn + 1;
+                    if (item.sellIn <= 0 && item.quality < 50) {
+                        item.sellIn = item.sellIn + 1;
                     }
                 }
             }
             //BACKSTAGE PASSES
-            if (itemType === backstagePasses) {
-                if (this.items[i].quality < 50 && this.items[i].sellIn >= 0) {
-                    this.increaseQualityByOne(i);
-                    if (this.items[i].quality < 50 && this.items[i].sellIn < 11) {
-                        this.increaseQualityByOne(i);
-                        if (this.items[i].quality < 50 && this.items[i].sellIn < 6) {
-                            this.increaseQualityByOne(i);
+            if (itemType == backstagePasses) {
+                if (item.quality < 50 && item.sellIn >= 0) {
+                    item.sellIn = item.sellIn + 1;
+                    if (item.quality < 50 && item.sellIn < 11) {
+                        item.sellIn = item.sellIn + 1;
+                        if (item.quality < 50 && item.sellIn < 6) {
+                            item.sellIn = item.sellIn + 1;
                         }
                     }
-                } else if (this.items[i].sellIn < 0) {
-                    this.items[i].quality = 0;
+                } else if (item.sellIn < 0) {
+                    item.quality = 0;
                 }
             }
             //MISC ITEMS
             if (itemType !== backstagePasses && itemType !== agedBrie && itemType !== sulfuras) {
-                if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1;
-                    if (this.items[i].sellIn <= 0 && this.items[i].quality > 0) {
-                        this.items[i].quality = this.items[i].quality - 1; 
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                    if (item.sellIn <= 0 && item.quality > 0) {
+                        item.quality = item.quality - 1; 
                     }
                 } 
             }
-            this.decreaseSellInByOne(i);
-        }
+            item.sellIn = item.sellIn - 1;
+        })
         return this.items;
     }
 }
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,13 +1,34 @@
 import { Item, GildedRose } from '../app/gilded-rose';
 
 const items: Array<Item> = [
-    new Item('Item 8', 1, 1),
+    new Item('Item1', 5, 2),
+            new Item('Item2', 1, 10),
+            new Item('Item3', 2, 10),
+            new Item('Item4', 4, 20),
+            new Item('Item5', -1, 4),
+            new Item('Item6', -1, 10),
+            new Item('Item 7', 3, 10),
+            new Item('Item 8', 1, 1),
+            new Item('Aged Brie', 1, 10),
+            new Item('Aged Brie', 2, 20),
+            new Item('Aged Brie', 0, 10),
+            new Item('Aged Brie', 5, 30),
+            new Item('Aged Brie', -1, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 4),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 4, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 6, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 3, 46),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 30),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 50),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 35),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Sulfuras, Hand of Ragnaros', 10, 80)
 ];
 
 const gildedRose = new GildedRose(items)
 gildedRose.updateQuality();
 gildedRose.updateQuality();
 gildedRose.updateQuality();
-
 
 console.log(gildedRose.items);
